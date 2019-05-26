1d4999aa596209060504a170e56979dbd3efa6e8
Arun R kumar <smartboyarun56@gmail.com>
2019-05-26 16:23:18 +0530

    Play/Pause Button Fixed and Updated

--- player.py
+++ player.py
@@ -4,6 +4,9 @@ from tkinter import filedialog
 from os import listdir
 from os.path import  join
 
+
+playing=False
+first_time=True
 window=Tk()
 
 index=0
@@ -25,9 +28,31 @@ def start():
     global index
     global player
     global song_name
+    global playing
+    global first_time
     song_name.set(onlyfiles[index][:-4])
     player=MediaPlayer(join(file_dir,onlyfiles[index]))
     player.play()
+    playing=True
+    first_time=False
+def player_play():
+    global index
+    global player
+    global song_name
+    global playing
+    global first_time
+
+    if(first_time):
+        first_time=False
+        start()
+    elif(playing):
+        playing=False
+        swap_icon(playing)
+        player.pause()
+    else:
+        playing=True
+        swap_icon(playing)
+        player.play()
 def next():
     global index
     global player
@@ -38,9 +63,20 @@ def prev():
     global index
     global player
     player.stop()
-    index+=1
+    index-=1
     start()
-
+def swap_icon(temp):
+    global varPh
+    global pausePh
+    global playPh
+    if (temp):
+        varPh = pausePh
+        button2.configure(image=varPh)
+        button2.image = varPh
+    else:
+        varPh = playPh
+        button2.configure(image=varPh)
+        button2.image = varPh
 window.title("MP3 Player")
 window.configure(background="black")
 window.geometry("300x200")
@@ -61,12 +97,16 @@ browse_button.pack(fill=X)
 playPh = PhotoImage(file='C:\\Users\\JAYATV\\PycharmProjects\\MusicPlayer\\Images\\play.png')
 nextPh = PhotoImage(file='C:\\Users\\JAYATV\\PycharmProjects\\MusicPlayer\\Images\\next.png')
 prevPh = PhotoImage(file='C:\\Users\\JAYATV\\PycharmProjects\\MusicPlayer\\Images\\previous.png')
+pausePh= PhotoImage(file='C:\\Users\\JAYATV\\PycharmProjects\\MusicPlayer\\Images\\pause.png')
+
+varPh=PhotoImage()
+
 
 button1=Button(bottomFrame,image=prevPh,bg='black',height=40,width=40,command=prev)
-button2=Button(bottomFrame,image=playPh,bg='black',height=40,width=40,command=start)
+button2=Button(bottomFrame,image=varPh,bg='black',height=40,width=40,command=player_play)
 button3=Button(bottomFrame,image=nextPh,bg='black',height=40,width=40,command=next)
 button1.image=prevPh
-button2.image=playPh
+button2.image=varPh
 button3.image=nextPh
 
 
